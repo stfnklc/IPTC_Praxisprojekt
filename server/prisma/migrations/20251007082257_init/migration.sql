061499306f20914ae910aaa6d80e02ea02843e64
Stefan Nikolic <stefan.nikolic@smail.th-koeln.de>
2025-10-15 08:47:15 +0200

    Cleanup für Restart mit Vanilla TS

--- server/prisma/migrations/20251007082257_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Image" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "filename" TEXT NOT NULL,
-    "originalName" TEXT NOT NULL,
-    "mimeType" TEXT NOT NULL,
-    "sizeBytes" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
